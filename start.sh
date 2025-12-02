3068242ec767840a4fda860701cb4c255152e16b
LoicAmstutz <117356070+LoicAmstutz@users.noreply.github.com>
2025-12-02 09:38:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,12 @@ docker pull mongo
 
 docker pull mongo-express
 
+docker network create mongo-network
+
+echo "für Mongo-Datenbank"
+docker run --name mongodb -p27017:27017 -d -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=Welcome$21 --net mongo-network mongo
+
+echo "für UI von Mongo-Datenbank"
+docker run -d -p8081:8081 -e ME_CONFIG_MONGODB_ADMINUSERNAME=admin -e ME_CONFIG_MONGODB_ADMINPASSWORD=Welcome$21 -e ME_CONFIG_MONGODB_SERVER=mongodb --net mongo-network --name mongo-express mongo-express
+
 docker ps
