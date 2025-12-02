3d3464f5f9f2179b8cb5e49f4d9df4680019d40c
LoicAmstutz <117356070+LoicAmstutz@users.noreply.github.com>
2025-12-02 09:49:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,22 @@
 #für Bash Skript:
 docker login -u lam132363
 
-docker run -p6000:6379 -d --name redis_new redis
+#docker run -p6000:6379 -d --name redis_new redis
 
-docker run -p6001:6379 -d --name redis_old redis:8.2
+#docker run -p6001:6379 -d --name redis_old redis:8.2
 
+echo "\n Pull Requests"
 docker pull mongo
 
 docker pull mongo-express
 
+echo "\n create network"
 docker network create mongo-network
 
-echo "für Mongo-Datenbank"
-docker run --name mongodb -p27017:27017 -d -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=Welcome$21 --net mongo-network mongo
+echo "\n für Mongo-Datenbank"
+docker run --name mongodb -p 27017:27017 -d -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=Welcome$21 --net mongo-network mongo
 
-echo "für UI von Mongo-Datenbank"
-docker run -d -p8081:8081 -e ME_CONFIG_MONGODB_ADMINUSERNAME=admin -e ME_CONFIG_MONGODB_ADMINPASSWORD=Welcome$21 -e ME_CONFIG_MONGODB_SERVER=mongodb --net mongo-network --name mongo-express mongo-express
+echo "\n für UI von Mongo-Datenbank"
+docker run -d -p 8081:8081 -e ME_CONFIG_MONGODB_ADMINUSERNAME=admin -e ME_CONFIG_MONGODB_ADMINPASSWORD=Welcome$21 -e ME_CONFIG_MONGODB_SERVER=mongodb --net mongo-network --name mongo-express mongo-express
 
 docker ps
